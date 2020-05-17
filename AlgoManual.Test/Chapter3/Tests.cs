c590b9af92a1b9e3b1bed101af3619efab94135b
Gaurav Personal <kinglygaurav@gmail.com>
2020-05-17 16:36:21 -0500

    Initial Commit

---
+++ AlgoManual.Test/Chapter3/Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace AlgoManual.Test.Chapter3
+{
+    public class Chapter3Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
