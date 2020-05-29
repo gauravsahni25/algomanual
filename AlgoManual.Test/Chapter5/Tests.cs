153c302fa7300f7bd8d43e917d7c302096d16bb7
Gaurav Personal <kinglygaurav@gmail.com>
2020-05-29 16:00:35 -0500

    Added Graph as per book

---
+++ AlgoManual.Test/Chapter5/Tests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using AlgoManual.Chapter5;
+using NUnit.Framework;
+
+namespace AlgoManual.Test.Chapter5
+{
+    [TestFixture]
+    public class Tests
+    {
+        private List<Point> points;
+        Graph graph = new Graph(5);
+        [SetUp]
+        public void Setup()
+        {
+            points = new List<Point>()
+            {
+                new Point(1,2),
+                new Point(2,3),
+                new Point(3,4),
+                new Point(4,1),
+                new Point(1,3)
+            };
+        }
+
+        [Test]
+        public void PrintGraph()
+        {
+            graph.ReadGraph(points, false);
+            graph.NVertices = 5;
+            graph.PrintGraph();
+            Console.WriteLine("-- End of Test--");
+        }
+
+        [Test]
+        public void WhenUndirected_For_12Edge_21Edge_IsAlsoPresent()
+        {
+            graph.ReadGraph(points, false);
+            bool found = false;
+            EdgeNode edge = graph.Edges[2];
+            while (edge!=null && found == false)
+            {
+                if (edge.YValue == 1)
+                {
+                    found = true;
+                }
+                else
+                    edge = edge.Next;
+            }
+            Assert.IsTrue(found);
+        }
+
+        [Test]
+        public void WhenDirected_For_12Edge_21Edge_IsNotPresent()
+        {
+            graph.ReadGraph(points, true);
+            bool found = false;
+            EdgeNode edge = graph.Edges[2];
+            while (edge != null && found == false)
+            {
+                if (edge.YValue == 1)
+                {
+                    found = true;
+                }
+                else
+                    edge = edge.Next;
+            }
+            Assert.IsFalse(found);
+        }
+
+        [Test]
+        public void WhenDirected_Total_NumberOfEdges_5()
+        {
+            graph.ReadGraph(points, true);
+            
+            Assert.IsTrue(graph.NEdges == 5);
+        }
+
+        [Test]
+        public void WhenUnDirected_Total_NumberOfEdges_10()
+        {
+            graph.ReadGraph(points, false);
+
+            Assert.IsTrue(graph.NEdges == 10);
+        }
+
+        [Test]
+        public void Total_NumberOfVertices_Setup_Correcty()
+        {
+            graph.ReadGraph(points, false);
+
+            Assert.IsTrue(graph.NVertices == 5);
+        }
+    }
+}
---
+++ AlgoManual/Chapter5/Graph.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+
+namespace AlgoManual.Chapter5
+{
+    [DebuggerDisplay("Edge: ({XValue} - {YValue})")]
+    public class EdgeNode
+    {
+        public int YValue { get; set; }
+        public int? Weight { get; set; }
+        public EdgeNode Next { get; set; }
+        public int XValue { get; set; }
+    }
+    
+
+    //Adjacency List
+    public class Graph
+    {
+        public Graph(int MAXV)
+        {
+            Edges = new EdgeNode[MAXV];
+            Degree = new int[MAXV];
+            MaxV = MAXV;
+        }
+
+        public int MaxV { get; set; }
+        // Array of Linked Lists, denotes adj. list of vertex at a given index.
+        public EdgeNode[] Edges { get; set; }
+        // Array of degree, denotes degree of vertex at a given index
+        public int[] Degree { get; set; } 
+        public int NVertices { get; set; }
+        public int NEdges { get; set; }
+        public bool Directed { get; set; }
+
+        public void ReadGraph(List<Point> edges, bool directed)
+        {
+            InitializeGraph(directed);
+            // NEdges = edges.Count;
+
+            for (int i = 0; i < edges.Count; i++)
+            {
+                InsertEdge(edges[i].X, edges[i].Y, directed);
+            }
+        }
+        private void InitializeGraph(bool directed)
+        {
+            NVertices = 0;
+            NEdges = 0;
+            Directed = directed;
+            for (int i = 0; i < MaxV; i++)
+            {
+                Degree[i] = 0;
+            }
+
+            for (int i = 0; i < MaxV; i++)
+            {
+                Edges[i] = null;
+            }
+        }
+
+        //We parametrize our insertion with the directed Boolean flag
+        // This is to identify whether we need to insert two copies of each edge or only one.
+        // Because Edges by themselves are always directional in Adjacency List
+        // So if (x,y) is an edge, in Undirected we need to add (y,x) too
+        private void InsertEdge(int x, int y, bool directed)
+        {
+            // create a node | We represent | Directed Edge (x, y) by an EdgeNode y in x's Adjacency List
+            EdgeNode edgeNode = new EdgeNode
+            {
+                // point the new Node to Edges[x]'s linked list
+                Next = Edges[x],
+                YValue =y,
+                Weight = null,
+
+                XValue= x
+            };
+
+           // Put the new node at the HEAD of the Adjacency Linked list
+            Edges[x] = edgeNode;
+
+            // Increase the degree
+            Degree[x] = Degree[x] + 1;
+            NEdges++;
+
+            if (!directed)
+            {
+                // The same edge has to appear in Y's Adjacency List | Thus graph is Undirected
+                InsertEdge(y, x, true);
+            }
+            else
+            {
+                
+            }
+        }
+
+        public void PrintGraph()
+        {
+            for (int i = 0; i < NVertices; i++)
+            {
+                Console.WriteLine(i);
+                EdgeNode edge = this.Edges[i];
+                while (edge!=null)
+                {
+                    Console.WriteLine($"{edge.YValue}");
+                    edge = edge.Next;
+                }
+                Console.WriteLine("--");
+            }
+        }
+    }
+}
