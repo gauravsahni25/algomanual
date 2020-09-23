4ce6f37db84244fb180aeb87863cc8d413967a46
Gaurav Sahni <gauravs@blinds.com>
2020-09-22 20:28:40 -0500

    Union Find

---
+++ AlgoManual/Chapter6/MinimumSpanningTree/Kruskal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AlgoManual.Chapter5;
+
+namespace AlgoManual.Chapter6.MinimumSpanningTree
+{
+    public class Kruskal
+    {
+        public Graph CandidateGraph { get; }
+        public Kruskal(Graph candidateGraph)
+        {
+            CandidateGraph = candidateGraph;
+        }
+
+        public void Execute()
+        {
+            EdgeNode[] edgePair = new EdgeNode[CandidateGraph.MaxV];
+
+        }
+    }
+}
---
+++ AlgoManual/Chapter6/MinimumSpanningTree/UnionFind.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoManual.Chapter6.MinimumSpanningTree
+{
+    class UnionFind
+    {
+    }
+
+    public class SetUnion
+    {
+        public int[] Parent { get; set; }
+        public int[] Size { get; set; }
+        public int NumberOfElements { get; set; }
+        
+        
+        public SetUnion(int n) // set_union_init (p-200)
+        {
+            Parent = new int[n];
+            Size = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Parent[i] = i;
+                Size[i] = 1;
+            }
+
+            NumberOfElements = n;
+        }
+
+        public int Find(int x)
+        {
+            if (Parent[x] == x)
+            {
+                return x;
+            }
+
+            return Find(Parent[x]);
+        }
+
+        public void UnionSets(int s1, int s2)
+        {
+            var root1 = Find(s1);
+            var root2 = Find(s2);
+
+            if (root1 == root2)
+            {
+                return; // nothing to union, already same connected component
+            }
+
+            if (Size[root1] > Size[root2])
+            {
+                Size[root1] = Size[root1] + Size[root2];
+                Parent[root2] = root1;
+            }
+            else
+            {
+                Size[root2] = Size[root1] + Size[root2];
+                Parent[root1] = root2;
+            }
+        }
+
+        public bool IsSameComponent(int s1, int s2) => Find(s1) == Find(s2);
+    }
+}
